96f363905eb2907d766747420e8146c5e756c9eb
amitoshraj911 <114009449+Amitosh-Raj@users.noreply.github.com>
2025-01-18 05:17:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+     + 
+     3)  // code readability should be high
+
+console.log("Amitosh")
+
+
+let name = "Amitosh"
+let age = 20
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value, typeof null is Object(Interview Question)
+// undefined => abhi value asign nhi hui h
+// symbol => uniqe
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
