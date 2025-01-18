ba255ff77614ef1b3fa1676cf3b92cf74b563eff
amitoshraj911 <114009449+Amitosh-Raj@users.noreply.github.com>
2025-01-18 04:15:26 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "amitoshraj911@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountstate;
+
+//accountId = 2  // not allowed
+accountEmail = "hc@gc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountstate])
