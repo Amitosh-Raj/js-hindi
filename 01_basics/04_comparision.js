4ca5225bc5a16f1b9086a3e633209b3ca6ac7aa8
amitoshraj911 <114009449+Amitosh-Raj@users.noreply.github.com>
2025-01-19 06:05:59 +0000

    comparisons

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+
+// ===  -> strict check, this is used to not only comparing the value but also check the data Types
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
