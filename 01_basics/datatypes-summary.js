11c4d965f4658aaa401c713fd978684bec949976
amitoshraj911 <114009449+Amitosh-Raj@users.noreply.github.com>
2025-01-19 06:38:46 +0000

    DataType Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+//const bignumber = 8383232948732949n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Amitosh",
+    age: 22,
+}
+
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherid);
