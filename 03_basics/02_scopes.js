ff6f4ead4def10ab445879bdc1710a9ac9b11421
amitoshraj911 <114009449+Amitosh-Raj@users.noreply.github.com>
2025-01-25 03:38:25 +0000

    scopes and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,10 +4,58 @@ if(true)
 {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    //console.log("INNER: ", a);
 }
 
 
-console.log(a);
+// console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one()
+{
+    const username = "Amitosh"
+
+    function two()
+    {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+if(true)
+{
+    const username = "Amitosh"
+    if(username == "Amitosh")
+    {
+        const website = " youtube"
+        //console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+// +++++++++ Intresting +++++++++
+
+console.log(addone(5));
+function addone(num)
+{
+    return num+1
+}
+
+addTwo(5)
+const addTwo = function(num)
+{
+    return num+2
+}
